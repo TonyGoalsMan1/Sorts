e40f3e324519f202271261a4d32984ea284b79bd
TonyN7 <82488453+TonyN7@users.noreply.github.com>
2022-04-27 00:00:20 +0300

    sorting

---
+++ sorting_alg.h/sorting_alg.h/sorting_alg.h
@@ -0,0 +1,257 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+// функция сортировки
+typedef struct SortFunc {
+    void (*sort) (int* a, size_t n);
+    char name[64];
+} SortFunc;
+
+typedef struct nSortFunc {
+    long long int (*sort) (int* a, size_t n);
+    char name[64];
+} SortFunc;
+
+// функция генерации
+typedef struct GeneratingFunc {
+    void (*generate) (int* a, size_t n);
+
+    char name[64];
+
+} GeneratingFunc;
+
+void swap(int* num1, int* num2) {
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+
+int index_of_min(int a[], int low, int high) {
+    int lowind = low, i; //two vars are declared, the first is initialized, the second is not
+    //int lowind, i;
+    //lowind = i;
+    for (i = low + 1; i <= high; i++) {   /* check for smaller elements from low+1 to high */
+        if (a[i] < a[lowind])
+            lowind = i;
+    }
+    return lowind;
+}
+
+void selection_sort(int* a, size_t n) {
+    int lowind, i, j;
+    for (i = 0; i < n - 1; i++) {   /* number of passes through a */
+        lowind = index_of_min(a, i, n - 1);
+        if (i != lowind)               /* swap if lowind not the same as position i */
+            swap(&a[i], &a[lowind]);
+    }
+}
+
+
+
+void bubble(int* a, size_t n) // function to implement bubble sort  
+{
+    int i, j, temp;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[i])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+
+void generateRandomArray(int* a, size_t n) {
+
+    srand(time(0));
+
+    for (int i = 0; i < n; i++)
+        a[i] = rand() % 10000;
+}
+
+void generateOrderedArray(int* a, size_t n) {
+
+    for (int i = 0; i < n; i++)
+        a[i] = i;
+
+}
+
+void generateOrderedBackwards(int* a, size_t n) {
+
+    for (int i = n; i >= 0; i--)
+        a[i] = i;
+}
+
+
+void generateRandomPosNeg(int* a, size_t n) {
+
+    srand(time(0));
+
+    for (int i = 0; i < n; i++)
+        a[i] = rand() % 20000 - 10000;
+}
+
+// Quick sort in C
+
+
+// function to find the partition position
+int partition(int array[], int low, int high) {
+
+    // select the rightmost element as pivot
+    int pivot = array[high];
+
+    // pointer for greater element
+    int i = (low - 1);
+
+    // traverse each element of the array
+    // compare them with the pivot
+    for (int j = low; j < high; j++) {
+        //Count comparisons nComp
+        if (array[j] <= pivot) {
+
+            // if element smaller than pivot is found
+            // swap it with the greater element pointed by i
+            i++;
+
+            // swap element at i with element at j
+            swap(&array[i], &array[j]);
+        }
+    }
+
+    // swap the pivot element with the greater element at i
+    swap(&array[i + 1], &array[high]);
+
+    // return the partition point
+    return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+    if (low < high) {
+
+        // find the pivot element such that
+        // elements smaller than pivot are on left of pivot
+        // elements greater than pivot are on right of pivot
+        int pi = partition(array, low, high);
+
+        // recursive call on the left of pivot
+        quickSort(array, low, pi - 1);
+
+        // recursive call on the right of pivot
+        quickSort(array, pi + 1, high);
+    }
+}
+
+void qSort(int* a, size_t n) {
+
+    quickSort(a, 0, n - 1);
+}
+
+// function to print array elements
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
+
+/*
+// main function
+int main() {
+  int data[] = {8, 7, 2, 1, 0, 9, 6};
+
+  int n = sizeof(data) / sizeof(data[0]);
+
+  printf("Unsorted Array\n");
+  printArray(data, n);
+
+  // perform quicksort on data
+  quickSort(data, 0, n - 1);
+
+  printf("Sorted array in ascending order: \n");
+  printArray(data, n);
+}
+*/
+
+void shellSort(int* intArray, size_t elements) {
+    int inner, outer;
+    int valueToInsert;
+    int interval = 1;
+    int i = 0;
+
+    while (interval <= elements / 3) {
+        interval = interval * 3 + 1;
+    }
+
+    while (interval > 0) {
+        //printf("iteration %d#:",i); 
+        //display();
+
+        for (outer = interval; outer < elements; outer++) {
+            valueToInsert = intArray[outer];
+            inner = outer;
+
+            while (inner > interval - 1 && intArray[inner - interval]
+                >= valueToInsert) {
+                intArray[inner] = intArray[inner - interval];
+                inner -= interval;
+                //printf(" item moved :%d\n",intArray[inner]);
+            }
+
+            intArray[inner] = valueToInsert;
+            //printf(" item inserted :%d, at position :%d\n",valueToInsert,inner);
+        }
+
+        interval = (interval - 1) / 3;
+        i++;
+    }
+}
+
+/* Function to sort an array using insertion sort*/
+void insertionSort(int* arr, size_t n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void Combsort(int* a, size_t nElements)
+{
+    int i, j, gap, swapped = 1;
+    int temp;
+
+    gap = nElements;
+    while (gap > 1 || swapped == 1)
+    {
+        gap = gap * 10 / 13;
+        if (gap == 9 || gap == 10) gap = 11;
+        if (gap < 1) gap = 1;
+        swapped = 0;
+        for (i = 0, j = gap; j < nElements; i++, j++)
+        {
+            if (a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                swapped = 1;
+            }
+        }
+    }
+}
---
+++ time_experiments/time_experiments/time_experiments.cpp
@@ -0,0 +1,153 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include "C:\Users\User\OneDrive\Рабочий стол\5c\sorting_alg.h\sorting_alg.h\sorting_alg.h"
+
+#define ARRAY_SIZE(array) sizeof (array) / sizeof (array [0])
+//No white space in macro function header!
+#define TIME_TEST(testCode,time) { \
+clock_t start_time = clock () ; \
+testCode \
+clock_t end_time = clock () ;\
+clock_t sort_time = end_time - start_time ; \
+time = ( double ) sort_time / CLOCKS_PER_SEC ; \
+}
+
+
+void checkTime(void (*sortFunc) (int*, size_t),
+    void (*generateFunc) (int*, size_t),
+    size_t size, char* experimentName);
+
+int isOrdered(int*, size_t size);
+
+void outputArray_(int* array, size_t size);
+
+
+double getTime() {
+    clock_t start_time = clock();
+    // фрагмент кода
+    // время которого измеряется
+    clock_t end_time = clock(); \
+        clock_t sort_time = end_time - start_time;
+    return (double)sort_time / CLOCKS_PER_SEC;
+}
+
+
+void timeExperiment() {
+    // описание функций сортировки
+    SortFunc sorts[] = {
+    { selection_sort, "Selection_Sort" } ,
+    { bubble, "Bubble_Sort" } ,
+    // вы добавите свои сортировки
+    {qSort, "Quick_Sort"},
+    {shellSort, "Shell_Sort"},
+    {insertionSort, "Insertion_Sort"},
+    {Combsort, "Comb_Sort"}
+    };
+    const unsigned FUNCS_N = ARRAY_SIZE(sorts);
+
+    // описание функций генерации
+    GeneratingFunc generatingFuncs[] = {
+        // генерируется случайный массив
+        { generateRandomArray,    "_random_" } ,
+        // генерируется массив 0, 1, 2, ..., n - 1
+        { generateOrderedArray , "_ordered_" } ,
+        // генерируется массив n - 1, n - 2, ..., 0
+        { generateOrderedBackwards , "_orderedBackwards_" },
+        { generateRandomPosNeg, "_mixed_pos_neg_"}
+    };
+    const unsigned CASES_N = ARRAY_SIZE(generatingFuncs);
+
+
+    // запись статистики в файл
+    for (size_t size = 1000; size <= 10000; size += 1000) {
+        printf(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n ");
+        printf(" Size : %ld \n ", size);
+        for (int i = 0; i < FUNCS_N; i++) {
+            for (int j = 0; j < CASES_N; j++) {
+                // генерация имени файла
+                static char filename[256];
+                sprintf(filename, " %s_ %s_time ", sorts[i].name, generatingFuncs[j].name);
+                checkTime(sorts[i].sort, generatingFuncs[j].generate, size, filename);
+            }
+        }
+        printf("\n");
+
+    }
+
+}
+
+
+void checkTime(void (*sortFunc) (int*, size_t),
+    void (*generateFunc) (int*, size_t),
+    size_t size, char* experimentName) {
+
+    static size_t runCounter = 1;
+    // генерация последовательности
+    static int innerBuffer[100000];
+    generateFunc(innerBuffer, size);
+    printf(" Run #%zu | ", runCounter++);
+    printf(" Name : %s \n ", experimentName);
+    // замер времени
+    double time;
+    TIME_TEST({ sortFunc(innerBuffer,size); }, time);
+
+    // результаты замера
+    printf(" Status : ");
+    if (isOrdered(innerBuffer, size)) {
+        printf(" OK ! Time : %.3f s .\n ", time);
+        // запись в файл
+        char filename[256];
+        sprintf(filename, "./data/%s.csv", experimentName);
+        FILE* f = fopen(filename, "a");
+        if (f == NULL) {
+            printf(" FileOpenError %s ", filename);
+            exit(1);
+        }
+        fprintf(f, "%zu;%.3f\n", size, time);
+        fclose(f);
+    }
+    else {
+        printf(" Wrong !\n ");
+        // вывод массива, который не смог быть отсортирован
+        outputArray_(innerBuffer, size);
+        exit(1);
+    }
+
+} //end of checkTime() function
+
+
+int isOrdered(int* array, size_t size) {
+
+    //return 0 or 1
+
+    for (int i = 0; i < size - 1; i++)
+        if (array[i] > array[i + 1])
+            return 0;
+
+    return 1;
+}
+
+void outputArray_(int* array, size_t size) {
+
+    for (int i = 0; i < size; i++)
+        printf("%d ", array[i]);
+
+    printf(" \n");
+
+}
+
+
+
+
+
+
+
+int main() {
+
+    timeExperiment();
+
+    return 0;
+
+}
